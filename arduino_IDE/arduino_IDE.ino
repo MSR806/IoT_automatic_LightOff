dae01728b17e689c2bdb0faf27d9fb39e82b6dab
MSR806 <msr0806@users.noreply.github.com>
2020-12-06 14:42:44 +0530

    version 1 not working properly

--- arduino_IDE/arduino_IDE.ino
+++ arduino_IDE/arduino_IDE.ino
@@ -3,6 +3,9 @@
 HC_SR04 outside(12, 13);
 HC_SR04 inside(7, 6);
 
+long outside_read, inside_read;
+int count = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -16,22 +19,28 @@ void loop() {
   inside.read();
   outside.set_threshold(3900);
   inside.set_threshold(3900);
-  before_while = mills();
-  while (outside.if_crossed())
-  { 
-    
-    if(inside.if_crossed())
-    {
-      Serial.println("Entered room");
-    }
+  
+  outside_read = 1000;
+  inside_read = 10000;
+
+  if (outside.if_crossed()){
+    outside_read = millis();
+    Serial.println("outside");
+  }
+  if (inside.if_crossed()){
+    inside_read = millis();
+    Serial.println("Inside");
   }
 
-  if (inside.if_crossed())
-  {
-    delay(200);
-    if(outside.if_crossed())
-    {
-      Serial.println("Left room");
-    }
+  if ((outside_read - inside_read) < 1000 && (outside_read - inside_read) > 0){
+    Serial.println("Exited the room");
+    count --;
+    Serial.println(count);
+  }
+
+  if ((inside_read - outside_read) < 1000 && (inside_read - outside_read) > 0){
+    Serial.println("Entered the room");
+    count ++;
+    Serial.println(count);
   }
 }
