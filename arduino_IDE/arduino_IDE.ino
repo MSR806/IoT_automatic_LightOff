cd298fac9f96df52f6de2c7fa99ed8c7f0d9a654
MSR806 <msr0806@users.noreply.github.com>
2020-12-17 06:02:05 +0530

    New logic- working fine

--- arduino_IDE/arduino_IDE.ino
+++ arduino_IDE/arduino_IDE.ino
@@ -3,7 +3,14 @@
 HC_SR04 outside(12, 13);
 HC_SR04 inside(7, 6);
 
-long outside_read, inside_read;
+bool inside_activated = false;
+bool inside_update = false;
+bool outside_activated = false;
+bool outside_update = false;
+
+long lastActive_outside = 0;
+long lastActive_inside = 0;
+long lastprint = 0;
 int count = 0;
 
 void setup() {
@@ -14,33 +21,66 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //reads Sensor Data
   outside.read();
   inside.read();
+
+  
   outside.set_threshold(3900);
   inside.set_threshold(3900);
   
-  outside_read = 1000;
-  inside_read = 10000;
+  /* Check if sensor_value is less than threshold
+   and if yes then sets activated variable to true for 1.5s */
+  long now = millis();
 
+  //for outside sensor
   if (outside.if_crossed()){
-    outside_read = millis();
-    Serial.println("outside");
+    outside_activated = true;
+    outside_update = true;
+    lastActive_outside = now;
   }
+  else if (now - lastActive_outside > 1500){
+    outside_activated = false;
+  }
+
+  //for inside sensor
   if (inside.if_crossed()){
-    inside_read = millis();
-    Serial.println("Inside");
+    inside_activated = true;
+    inside_update = true;
+    lastActive_inside = now;
+  }
+  else if(now - lastActive_inside > 1500){
+    inside_activated = false;
   }
 
-  if ((outside_read - inside_read) < 1000 && (outside_read - inside_read) > 0){
-    Serial.println("Exited the room");
-    count --;
-    Serial.println(count);
+  //if the sensor is in a activated state then do this
+  if (outside_activated && lastActive_inside - lastActive_outside > 0 && inside_update && outside_update){
+    if(inside_activated){
+      count ++;
+      inside_update = false;
+      outside_update = false;
+
+    }
   }
 
-  if ((inside_read - outside_read) < 1000 && (inside_read - outside_read) > 0){
-    Serial.println("Entered the room");
-    count ++;
-    Serial.println(count);
+  if (inside_activated && lastActive_outside -lastActive_inside > 0 && inside_update && outside_update){
+    if(outside_activated){
+      count --;
+      inside_update = false;
+      outside_update = false;
+    }
   }
+
+  
+  if(now - lastprint > 400){
+    Serial.print("Number of people = ");
+    Serial.println(count);
+    Serial.print("outside_activated = ");
+    Serial.println(outside_activated);
+    Serial.print("inside_activated = ");
+    Serial.println(inside_activated);
+    lastprint = now;
+  }
+  
 }
